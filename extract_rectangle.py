a8eb8f2b2ba7b20317bdaf42525816d42dca629f
Jacob Kingery <jacob.kingery@students.olin.edu>
2015-10-21 21:30:08 -0400

    adding initial rectangle extraction code

---
+++ extract_rectangle.py
@@ -0,0 +1,68 @@
+import cv2
+import numpy as np
+from sys import argv
+
+im = cv2.imread(argv[1])
+cv2.imshow("original", im)
+
+# BGR filtering, might not work as well for actual pictures in actual lighting conditions
+lower_bounds = (0, 200, 0)
+upper_bounds = (50, 255, 50)
+binary = cv2.inRange(im, lower_bounds, upper_bounds)
+cv2.imshow("binary", binary)
+
+# Edge detection
+edged = cv2.Canny(binary, 30, 200)
+cv2.imshow("edged", edged)
+
+# Locate contours and extract those with 4 vertices
+(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+rect_cnt = None
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+ 
+    if len(approx) == 4:
+        rect_cnt = approx
+        cv2.drawContours(im, [approx], -1, (255, 0, 0), 1)
+        break
+cv2.imshow("with rectangle", im)
+
+if rect_cnt is None:
+    print 'no rectangle contour found'
+    exit(0)
+
+# Order vertices as top-left, top-right, bottom-right, bottom-left
+pts = rect_cnt.reshape(4, 2)
+rect = np.zeros((4, 2), dtype = "float32")
+center = pts.mean(axis = 0)
+rect[:2] = sorted([pt for pt in pts if pt[1] < center[1]], key = lambda p: p[0])
+rect[2:] = sorted([pt for pt in pts if pt[1] >= center[1]], key = lambda p: p[0], reverse = True)
+
+# Calculate size of result, might be able to use this in the filtering of rectangles (by using a known h/w ratio)
+tl, tr, br, bl = rect
+width = int(max(
+    np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2)),
+    np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+))
+height = int(max( 
+    np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2)),
+    np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+))
+dst = np.array(
+    [
+        [0, 0],
+        [width - 1, 0],
+        [width - 1, height - 1],
+        [0, height - 1]
+    ],
+    dtype = "float32"
+)
+
+# Warp the image to un-perspective-ify the rectangle
+M = cv2.getPerspectiveTransform(rect, dst)
+warp = cv2.warpPerspective(im, M, (width, height))
+cv2.imshow("warped", warp)
+
+cv2.waitKey()
