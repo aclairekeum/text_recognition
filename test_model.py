40a049815cc8da7f9c051e03b41c82d0171e35eb
Jacob Kingery <jacob.kingery@students.olin.edu>
2015-10-26 16:42:28 -0400

    matched grayscale range to that of training data

--- test_model.py
+++ test_model.py
@@ -6,6 +6,8 @@ model = joblib.load('model/model.pkl')
 
 for arg in argv[1:]:
     im = cv2.imread(arg, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    im /= 16.0
+    im.round()
     resized = cv2.resize(im, (8, 8))
     data = resized.reshape((1,64))
 
