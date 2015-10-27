3b65fe780dbe3108d6da3abf31f64112ed4d916f
aclairekeum <ayoung.keum@students.olin.edu>
2015-10-27 11:03:09 -0400

    find square only

--- extract_rectangle.py
+++ extract_rectangle.py
@@ -21,10 +21,12 @@ gray_orig = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 fuzzed = cv2.bilateralFilter(im, 9, 75, 75)
 
 # BGR filtering, might not work as well for actual pictures in actual lighting conditions
-lower_bounds = (0, 0, 49)
-upper_bounds = (113, 69, 255)
-binary = cv2.inRange(fuzzed, lower_bounds, upper_bounds)
-cv2.imshow("binary", binary)
+# lower_bounds = (0, 0, 49)
+# upper_bounds = (113, 69, 255)
+# binary = cv2.inRange(fuzzed, lower_bounds, upper_bounds)
+# cv2.imshow("binary", binary)
+
+binary= gray_orig
 
 # Edge detection
 edged = cv2.Canny(binary, 0, 1)
@@ -33,6 +35,9 @@ cv2.imshow("edged", edged)
 # Locate contours and extract those with 4 vertices
 (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+
+
+
 rect_cnt = None
 for c in cnts:
     peri = cv2.arcLength(c, True)
@@ -40,41 +45,45 @@ for c in cnts:
     
     # Check for four sides and a reasonably large perimeter
     if len(approx) == 4 and peri > 300:
-        rect_cnt = approx
-        cv2.drawContours(im, [rect_cnt], -1, (0, 255, 0), 3)
-        break
+        # Order vertices as top-left, top-right, bottom-right, bottom-left
+        pts = approx.reshape(4, 2)
+        rect = np.zeros((4, 2), dtype = "float32")
+        center = pts.mean(axis = 0)
+        rect[:2] = sorted([pt for pt in pts if pt[1] < center[1]], key = lambda p: p[0])
+        rect[2:] = sorted([pt for pt in pts if pt[1] >= center[1]], key = lambda p: p[0], reverse = True)
+
+        # Calculate size of result, might be able to use this in the filtering of rectangles (by using a known h/w ratio)
+        tl, tr, br, bl = rect
+        width = int(max(
+            np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2)),
+            np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        ))
+        height = int(max( 
+            np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2)),
+            np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        ))
+        dst = np.array(
+            [
+                [0, 0],
+                [width - 1, 0],
+                [width - 1, height - 1],
+                [0, height - 1]
+            ],
+            dtype = "float32"
+        )
+        print "width = ", width, "height = ",height
+
+        if abs(width-height)<0.3*max(width, height):
+            rect_cnt = approx
+            cv2.drawContours(im, [rect_cnt], -1, (0, 255, 0), 3)
+            break
 
 if rect_cnt is None:
-    print "no rectangle contour found"
+    print "no square contour found"
     exit(0)
-cv2.imshow("with rectangle", im)
+cv2.imshow("with square", im)
 
-# Order vertices as top-left, top-right, bottom-right, bottom-left
-pts = rect_cnt.reshape(4, 2)
-rect = np.zeros((4, 2), dtype = "float32")
-center = pts.mean(axis = 0)
-rect[:2] = sorted([pt for pt in pts if pt[1] < center[1]], key = lambda p: p[0])
-rect[2:] = sorted([pt for pt in pts if pt[1] >= center[1]], key = lambda p: p[0], reverse = True)
 
-# Calculate size of result, might be able to use this in the filtering of rectangles (by using a known h/w ratio)
-tl, tr, br, bl = rect
-width = int(max(
-    np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2)),
-    np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-))
-height = int(max( 
-    np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2)),
-    np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-))
-dst = np.array(
-    [
-        [0, 0],
-        [width - 1, 0],
-        [width - 1, height - 1],
-        [0, height - 1]
-    ],
-    dtype = "float32"
-)
 
 # Warp the image to un-perspective-ify the rectangle
 M = cv2.getPerspectiveTransform(rect, dst)
